25e0a4e7842f74e7be06dae553a5e7933e92f883
johannesheininger <heininger.johannes@gmx.de>
2021-02-20 11:13:01 +0100

    Add first code with read csv and import pandas

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import pandas as pd
+
+
 def print_hi(name):
     print(f'Hi, {name}')
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+    stocks = pd.read_csv('files/sp_500_stocks.csv')
+    print(stocks.count())
